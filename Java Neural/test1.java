984d37eee62e05e38bcf281a70057659900a1500
Ayush Mann <rcbass.official@gmail.com>
2024-06-27 17:22:44 +0530

    .

--- Java Neural/test1.java
+++ Java Neural/test1.java
@@ -1,10 +1,4 @@
-/*
-Creates a basic neuron with 3 inputs.
-
-Associated YT NNFS tutorial: https://www.youtube.com/watch?v=Wo5dMEP_BbI
-*/
-
-class P001BasicNeuron3Inputs {
+class L1BasicNeuron3Inputs {
 	public static void main(String[] args) {
 		double[] inputs = {1.2, 5.1, 2.1};
 		double[] weights = {3.1, 2.1, 8.7};
